f07bc592c8ae96dd8ff512cc9a16627d16ca5702
Luiz Estrela <101803438+LuizEstrelaIII@users.noreply.github.com>
2023-10-26 19:55:46 -0300

    Commit Enumerações

---
+++ curso-java/Main.java
@@ -0,0 +1,15 @@
+import java.util.Date;
+
+public class Main {
+    public static void main(String [] args){
+        Order order = new Order(1080, new Date(), OrderStatus.pendind_payment);
+
+        System.out.println(order);
+
+        OrderStatus os1 = OrderStatus.delivered;
+        OrderStatus os2 = OrderStatus.valueOf("delivered");
+
+        System.out.println(os1);
+        System.out.println(os2);
+    }
+}
---
+++ curso-java/OrderStatus.java
@@ -0,0 +1,6 @@
+public enum OrderStatus {
+    pendind_payment,
+    processing,
+    shipped,
+    delivered;
+}
